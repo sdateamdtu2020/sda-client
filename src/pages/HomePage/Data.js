d0cbb974a73f01efcda56cfdd901afc5ae693068
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-14 04:26:54 +0700

    HomePage

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,52 @@
+export const homeObjOne = {
+	primary: true,
+	lightBg: false,
+	lightTopLine: false,
+	lightText: true,
+	lightTextDesc: true,
+	topLine: "Environment Analysis System",
+	headline: "Smart Dashboard Application",
+	description:
+		"We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.",
+	buttonLabel: "About Us",
+	urlLinkBtn: "/about",
+	imgStart: "",
+	img: require("../../assets/img/homepage.svg"),
+	alt: "Credit Card",
+	start: "",
+};
+
+export const homeObjTwo = {
+	primary: true,
+	lightBg: false,
+	lightTopLine: true,
+	lightText: true,
+	lightTextDesc: true,
+	topLine: "Smart Dashboard Application ",
+	headline: "What is SDA",
+	description:
+		"Means that a dashboard that is convenient for users to analyze and review data.",
+	buttonLabel: "Go to Dashboard",
+	urlLinkBtn: "/dashboard",
+	imgStart: "flex-end",
+	img: require("../../assets/img/what-is-sda.svg"),
+	alt: "Vault",
+	start: "",
+};
+
+export const homeObjThree = {
+	primary: true,
+	lightBg: false,
+	lightTopLine: true,
+	lightText: true,
+	lightTextDesc: true,
+	imgStart: "",
+	start: "",
+	topLine: "",
+	headline: "Need more information ?",
+	description: "Contact us if you need more information about our services",
+	buttonLabel: "Contact Us",
+	urlLinkBtn: "/contact",
+	img: require("../../assets/img/question.svg"),
+	alt: "question",
+};
