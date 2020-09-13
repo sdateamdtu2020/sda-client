2e6a5af7955da71edfff40e2949026768da4058b
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-09-14 04:26:33 +0700

    Contact page

---
+++ src/pages/Contact/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { InfoSection } from "../../components";
+import { homeObjOne, homeObjTwo } from "./Data";
+const Home = () => {
+	return (
+		<>
+			<InfoSection {...homeObjOne} />
+			<InfoSection {...homeObjTwo} />
+		</>
+	);
+};
+
+export default Home;
